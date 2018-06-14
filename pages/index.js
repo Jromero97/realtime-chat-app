57dbcdfd4826b415df9c7960fcaa4e558dd60fd9
JR Romero <jrromero@JRs-MacBook-Pro.local>
2018-06-14 03:14:05 -0600

    Finishin tutorial

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
-import React, {Component} from 'react';
-import Layout from 'components/Layout';
+
+import React, { Component } from 'react';
+import Layout from '../src/components/Layout';
+import Chat from '../src/components/Chat';
 
 class IndexPage extends Component {
 
-    state = {user: null};
+    state = { user: null };
 
     handleKeyUp = evt => {
         if (evt.keyCode === 13) {
-            const user = evt.target.value;
-            this.setState({user});
+            const user =  evt.target.value;
+            this.setState({ user });
         }
     };
 
     render() {
-        const {user} = this.state;
+        const { user } = this.state;
 
         const nameInputStyles = {
             background: 'transparent',
@@ -33,10 +35,9 @@ class IndexPage extends Component {
 
                     <div className="row position-absolute w-100 h-100">
 
-                        <section
-                            className="col-md-8 d-flex flex-row flex-wrap align-items-center align-content-center px-5">
+                        <section className="col-md-8 d-flex flex-row flex-wrap align-items-center align-content-center px-5">
                             <div className="px-5 mx-5">
-                  
+
                     <span className="d-block w-100 h1 text-light" style={{marginTop: -50}}>
                       {
                           user
@@ -47,16 +48,15 @@ class IndexPage extends Component {
                       }
                     </span>
 
-                                {!user &&
-                                <input type="text" className="form-control mt-3 px-3 py-2" onKeyUp={this.handleKeyUp}
-                                       autoComplete="off" style={nameInputStyles}/>}
+                                { !user && <input type="text" className="form-control mt-3 px-3 py-2" onKeyUp={this.handleKeyUp} autoComplete="off" style={nameInputStyles} /> }
 
                             </div>
                         </section>
 
-                        <section
-                            className="col-md-4 position-relative d-flex flex-wrap h-100 align-items-start align-content-between bg-white px-0">
+                        <section className="col-md-4 position-relative d-flex flex-wrap h-100 align-items-start align-content-between bg-white px-0">
+                            {user && <Chat activeUser={user}/>}
                         </section>
+
                     </div>
 
                 </main>
@@ -68,5 +68,5 @@ class IndexPage extends Component {
 }
 
 export default () => (
-    <IndexPage/>
+    <IndexPage />
 );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import IndexPage from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<IndexPage/>, document.getElementById('root'));
 registerServiceWorker();
